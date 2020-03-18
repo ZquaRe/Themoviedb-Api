31ddd2da8ae05bc42ee167f6f0a2eb3cd4608366
Furkan Sezgin <samp_leon@hotmail.com>
2020-03-18 05:40:36 +0200

    Bugs Fixed
    IMDB information error fixed.
    Recommendations bug fixed
    Movie / Series not found error fixed

--- src/class.themoviedb.php
+++ src/class.themoviedb.php
@@ -127,31 +127,45 @@ class TheMovieDb
 
         $MovResult = json_decode($this->request('search/multi?api_key=' . $this->API_KEY . '&language=' . $this->language . '&query=' . $this->movieRawName . '&page=' . $this->page . '&include_adult=false'));
 
-        $this->Mov_id = $MovResult->results[0]->id;
+        if (!empty($MovResult->results[0]->id)) {
 
-        if (!empty($MovResult->results[0]->title))
-            $this->Mov_title = $MovResult->results[0]->title;
-        else
-            $this->Mov_title = null;
 
-        if (!empty($MovResult->results[0]->name))
-            $this->Mov_name = $MovResult->results[0]->name;
-        else if (!empty($MovResult->results[0]->original_title))
-            $this->Mov_name = $MovResult->results[0]->original_title;
+            $this->Mov_id = $MovResult->results[0]->id;
 
-        $this->Mov_type = $MovResult->results[0]->media_type;
+            if (!empty($MovResult->results[0]->title))
+                $this->Mov_title = $MovResult->results[0]->title;
+            else
+                $this->Mov_title = null;
 
-        if (!empty($MovResult->results[0]->first_air_date))
-            $this->Mov_date = $MovResult->results[0]->first_air_date;
-        else if (!empty($MovResult->results[0]->release_date))
-            $this->Mov_date = $MovResult->results[0]->release_date;
+            if (!empty($MovResult->results[0]->name))
+                $this->Mov_name = $MovResult->results[0]->name;
+            else if (!empty($MovResult->results[0]->original_title))
+                $this->Mov_name = $MovResult->results[0]->original_title;
 
-        $this->Mov_overview = $MovResult->results[0]->overview;
-        $this->Mov_poster_path = $MovResult->results[0]->poster_path;
-        $this->Mov_images = 'https://image.tmdb.org/t/p/original' . $MovResult->results[0]->poster_path;
+            $this->Mov_type = $MovResult->results[0]->media_type;
 
-        return $MovResult;
+            if (!empty($MovResult->results[0]->first_air_date))
+                $this->Mov_date = $MovResult->results[0]->first_air_date;
+            else if (!empty($MovResult->results[0]->release_date))
+                $this->Mov_date = $MovResult->results[0]->release_date;
 
+            $this->Mov_overview = $MovResult->results[0]->overview;
+            $this->Mov_poster_path = $MovResult->results[0]->poster_path;
+            $this->Mov_images = 'https://image.tmdb.org/t/p/original' . $MovResult->results[0]->poster_path;
+
+            return $MovResult;
+        } else {
+            $this->Mov_images = null;
+            echo json_encode(
+                array(
+                    'status' => 'error',
+                    'description' => 'Movie or TV Series not found',
+                    'Url' => $this->url,
+                    'class' => get_class($this),
+                    'function' => __FUNCTION__
+                )
+            );
+        }
 
     }
 
@@ -160,7 +174,8 @@ class TheMovieDb
      */
     public function imdb_id()
     {
-        return $Mov_imdbid = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/external_ids?api_key=' . $this->API_KEY))->imdb_id;
+        if (!empty($this->Mov_id))
+            return $Mov_imdbid = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/external_ids?api_key=' . $this->API_KEY))->imdb_id;
     }
 
     /**
@@ -168,14 +183,19 @@ class TheMovieDb
      */
     public function imdb_ranked()
     {
-        $this->imdbid = self::imdb_id();
+        if (!empty($this->Mov_id)) {
 
-        preg_match_all('#<span class="small" itemprop="ratingCount">(.*)</span>#', self::request($this->imdbid . '/', true), $RatingCount);
-        preg_match_all('#<span itemprop="ratingValue">(.*)</span>#', self::request($this->imdbid . '/', true), $Result);
-        $this->imdb_rating = explode('/', strip_Tags($Result[0][0]))[0];
-        $this->imdb_bestRating = explode('/', strip_Tags($Result[0][0]))[1];
-        $this->imdb_ratingCount = $RatingCount[1][0];
-        return json_decode(json_encode(array('Imdbid' => $this->imdbid, 'Rating' => $this->imdb_rating, 'BestRating' => $this->imdb_bestRating, 'RatingTotalUserCount' => $this->imdb_ratingCount)));
+            $this->imdbid = self::imdb_id();
+            if (!empty($this->imdbid)) {
+
+                preg_match_all('#<span class="small" itemprop="ratingCount">(.*)</span>#', self::request($this->imdbid . '/', true), $RatingCount);
+                preg_match_all('#<span itemprop="ratingValue">(.*)</span>#', self::request($this->imdbid . '/', true), $Result);
+                $this->imdb_rating = explode('/', strip_Tags($Result[0][0]))[0];
+                $this->imdb_bestRating = explode('/', strip_Tags($Result[0][0]))[1];
+                $this->imdb_ratingCount = $RatingCount[1][0];
+                return json_decode(json_encode(array('Imdbid' => $this->imdbid, 'Rating' => $this->imdb_rating, 'BestRating' => $this->imdb_bestRating, 'RatingTotalUserCount' => $this->imdb_ratingCount)));
+            }
+        }
     }
 
     /**
@@ -183,8 +203,10 @@ class TheMovieDb
      */
     public function cast()
     {
-        //  https://api.themoviedb.org/3/tv/{tv_id}/credits?api_key={API_KEY}&language=tr
-        return $MovResult = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/credits?api_key=' . $this->API_KEY . '&language=tr'))->cast;
+        if (!empty($this->Mov_id))
+            //  https://api.themoviedb.org/3/tv/{tv_id}/credits?api_key={API_KEY}&language=tr
+            return $MovResult = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/credits?api_key=' . $this->API_KEY . '&language=tr'))->cast;
+
     }
 
     /**
@@ -192,8 +214,9 @@ class TheMovieDb
      */
     public function recommendations()
     {
-        //  https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={API_KEY}&language=tr&page=1
-        return $MovResult = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/recommendations?api_key=' . $this->API_KEY . '&language=tr&page=1'))->results;
+        if (!empty($this->Mov_id))
+            //  https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={API_KEY}&language=tr&page=1
+            return $MovResult = json_decode($this->request($this->Mov_type . '/' . $this->Mov_id . '/recommendations?api_key=' . $this->API_KEY . '&language=tr&page=1'))->results;
     }
 
     /**
@@ -201,11 +224,13 @@ class TheMovieDb
      */
     public function seasons()
     {
-        if ($this->Mov_type == 'tv') {
-            //https://api.themoviedb.org/3/tv/1396?api_key={API_KEY}&language=tr
-            return $MovResult = json_decode($this->request('tv/' . $this->Mov_id . '?api_key=' . $this->API_KEY . '&language=tr'))->seasons;
-        } else {
-            return null;
+        if (!empty($this->Mov_id)) {
+            if ($this->Mov_type == 'tv') {
+                //https://api.themoviedb.org/3/tv/1396?api_key={API_KEY}&language=tr
+                return $MovResult = json_decode($this->request('tv/' . $this->Mov_id . '?api_key=' . $this->API_KEY . '&language=tr'))->seasons;
+            } else {
+                return null;
+            }
         }
     }
 
@@ -215,70 +240,73 @@ class TheMovieDb
      */
     public function all()
     {
+        if (!empty($this->Mov_id)) {
 
+            foreach (self::cast() as $cast) {
 
-        foreach (self::cast() as $cast) {
-
-            $this->Cast[] = array(
-                'Cast_id' => $cast->id,
-                'Character' => $cast->character,
-                'Cast_name' => $cast->name,
-                'Cast_profilepath' => $cast->profile_path,
-                'Cast_profilepic' => 'https://image.tmdb.org/t/p/original' . $cast->profile_path
-            );
-        }
-
-
-        foreach (self::recommendations() as $recommendations) {
-            if (!empty($recommendations->original_title)) $recommendations->original_title = $recommendations->original_title; else $recommendations->original_title = $recommendations->name;
-            if (!empty($recommendations->release_date)) $recommendations->release_date = $recommendations->release_date; else $recommendations->release_date = null;
-
-            $this->Recommendations[] = array(
-                'Movie_id' => $recommendations->id,
-                'Mov_title' => $recommendations->original_title,
-                'Mov_overview' => $recommendations->overview,
-                'Mov_release_date' => $recommendations->release_date,
-                'Mov_poster_path' => $recommendations->poster_path,
-                'Mov_pic' => 'https://image.tmdb.org/t/p/original' . $recommendations->poster_path
-            );
-        }
-
-        if ($this->Mov_type == 'tv') {
-            foreach (self::seasons() as $seasons) {
-
-                $this->Seasons[] = array(
-                    'release_date' => $seasons->air_date,
-                    'episode_count' => $seasons->episode_count,
-                    'season_id' => $seasons->id,
-                    'overview' => $seasons->overview,
-                    'poster_path' => $seasons->poster_path,
-                    'poster_image' => 'https://image.tmdb.org/t/p/original' . $seasons->poster_path,
-                    'season_number' => $seasons->season_number,
-
+                $this->Cast[] = array(
+                    'Cast_id' => $cast->id,
+                    'Character' => $cast->character,
+                    'Cast_name' => $cast->name,
+                    'Cast_profilepath' => $cast->profile_path,
+                    'Cast_profilepic' => 'https://image.tmdb.org/t/p/original' . $cast->profile_path
                 );
             }
-        } else {
-            $this->Seasons = null;
+
+
+            foreach (self::recommendations() as $recommendations) {
+                if (!empty($recommendations->original_title)) $recommendations->original_title = $recommendations->original_title; else $recommendations->original_title = $recommendations->name;
+                if (!empty($recommendations->release_date)) $recommendations->release_date = $recommendations->release_date; else $recommendations->release_date = null;
+
+                $this->Recommendations[] = array(
+                    'Movies_id' => $recommendations->id,
+                    'Movies_title' => $recommendations->original_title,
+                    'Movies_overview' => $recommendations->overview,
+                    'Movies_release_date' => $recommendations->release_date,
+                    'Movies_poster_path' => $recommendations->poster_path,
+                    'Movies_pic' => 'https://image.tmdb.org/t/p/original' . $recommendations->poster_path
+                );
+            }
+
+            if ($this->Mov_type == 'tv') {
+                foreach (self::seasons() as $seasons) {
+
+                    $this->Seasons[] = array(
+                        'release_date' => $seasons->air_date,
+                        'episode_count' => $seasons->episode_count,
+                        'season_id' => $seasons->id,
+                        'overview' => $seasons->overview,
+                        'poster_path' => $seasons->poster_path,
+                        'poster_image' => 'https://image.tmdb.org/t/p/original' . $seasons->poster_path,
+                        'season_number' => $seasons->season_number,
+
+                    );
+                }
+            } else {
+                $this->Seasons = null;
+            }
+
+            if (empty($this->Recommendations)) $this->Recommendations = null;
+
+
+            return json_decode(json_encode(
+                array(
+                    'Movie_id' => $this->Mov_id,
+                    'Movie_name' => $this->Mov_name,
+                    'Movie_title' => $this->Mov_title,
+                    'Movie_type' => $this->Mov_type,
+                    'Movie_date' => $this->Mov_date,
+                    'Movie_overview' => $this->Mov_overview,
+                    'Movie_poster_path' => $this->Mov_poster_path,
+                    'Movie_images' => $this->Mov_images,
+                    'Imdb' => $this->imdb_ranked(),
+                    'Seasons' => $this->Seasons,
+                    'Cast' => $this->Cast,
+                    'Recommendations' => $this->Recommendations
+
+                )
+            ));
         }
-
-
-        return json_decode(json_encode(
-            array(
-                'Mov_id' => $this->Mov_id,
-                'Mov_name' => $this->Mov_name,
-                'Mov_title' => $this->Mov_title,
-                'Mov_type' => $this->Mov_type,
-                'Mov_date' => $this->Mov_date,
-                'Mov_overview' => $this->Mov_overview,
-                'Mov_poster_path' => $this->Mov_poster_path,
-                'Mov_images' => $this->Mov_images,
-                'imdb' => $this->imdb_ranked(),
-                'Seasons' => $this->Seasons,
-                'Cast' => $this->Cast,
-                'Recommendations' => $this->Recommendations
-
-            )
-        ));
     }
 
     /**
@@ -287,7 +315,8 @@ class TheMovieDb
      * @param null $name
      * @return string
      */
-    public function fileDownload($url, $folder, $name = null)
+    public
+    function fileDownload($url, $folder, $name = null)
     {
         $this->url = $url;
         $this->folder = $folder;
@@ -335,4 +364,5 @@ class TheMovieDb
         }
     }
 }
+
 ?>
